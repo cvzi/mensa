d5e989b3f80e853cc1432531a9d09a816bfd8ba6
cvzi <cuzi@openmail.cc>
2025-01-25 00:17:04 +0100

    strip html from names

--- mensenat/canteen.py
+++ mensenat/canteen.py
@@ -5,11 +5,12 @@ import json
 from pyopenmensa.feed import LazyBuilder
 import re
 
+
 class Canteen:
     def __init__(self, uri: str):
         match = re.search(r"https:\/\/www\.mensen\.at\/standort\/(.*?)(\/|$)", uri)
         if match:
-            self.location= match.group(1)
+            self.location = match.group(1)
         else:
             raise ValueError("No canteen id found in link: " + uri)
 
@@ -29,12 +30,12 @@ query Location($locationUri: String!) {
         transport = RequestsHTTPTransport(url="https://backend.mensen.at/api")
         client = Client(
             transport=transport,
-            fetch_schema_from_transport=False  # Disable schema fetching
+            fetch_schema_from_transport=False,  # Disable schema fetching
         )
 
         params = {
-                "locationUri": f"standort/{self.location}"
-            }
+            "locationUri": f"standort/{self.location}"
+        }
 
         result = client.execute(query, variable_values=params)
 
@@ -42,8 +43,8 @@ query Location($locationUri: String!) {
         menuPlanNextWeek = json.loads(result["nodeByUri"]["menuplanNextWeek"])
         weekMenus = {
             "menuplanCurrentWeek": menuplanCurrentWeek,
-            "menuplanNextWeek": menuPlanNextWeek
-            }
+            "menuplanNextWeek": menuPlanNextWeek,
+        }
 
         return weekMenus
 
@@ -61,45 +62,42 @@ query Location($locationUri: String!) {
         self.addWeekMenuToFeed(weekMenus["menuplanCurrentWeek"])
 
         return self.feed.toXMLFeed()
-    
 
     def addWeekMenuToFeed(self, weekMenu) -> str:
-        if weekMenu['available'] == False:
+        if weekMenu["available"] == False:
             return
-        
+
         firstDayOfTheWeek = weekMenu["first_day"]
-        
+
         for category in weekMenu["menus"]:
-            category_name = category["name"]
-            if category_name == '' or not 'menus' in category:
+            category_name = self.strip_html(category["name"])
+            if category_name == "" or not "menus" in category:
                 continue
 
             for weekday, menu in category["menus"].items():
                 current_date = self.calculateDateFromWeekdayNumberGiven(firstDayOfTheWeek, weekday)
                 self.addMealsToFeed(category_name, current_date, menu)
 
-
     def addMealsToFeed(self, category_name, current_date, meals):
         for meal in meals:
-            name = meal['title_de']
+            name = self.strip_html(meal['title_de'])
             allergens = meal['allergens']
             if isinstance(allergens, dict):
                 allergens = allergens.keys()
             notes = allergens
-            prices = {'other': meal['price']}
+            prices = {"other": meal["price"]}
             self.feed.addMeal(current_date, category_name, name, notes=notes, prices=prices)
-        
-    
 
     # for some reason, mensen.at doesnt provide the concrete date of the menu but the number of the week day of the current week.
     # To get the correct date we need to calculate the date again
     def calculateDateFromWeekdayNumberGiven(self, firstDayOfTheWeekStr: str, weekDayNumber: str) -> date:
         try:
-            firstDayOfTheWeek = datetime.strptime(firstDayOfTheWeekStr,"%Y-%m-%d").date()
+            firstDayOfTheWeek = datetime.strptime(firstDayOfTheWeekStr, "%Y-%m-%d").date()
             delta = int(weekDayNumber) - 1
             currentDate = firstDayOfTheWeek + timedelta(days=delta)
             return currentDate.strftime("%Y-%m-%d")
         except ValueError as e:
             raise ValueError(f"Failed to generate week day date: {e}") from e
 
-    
+    def strip_html(self, text):
+        return re.sub(" +", " ", re.sub("<[^<]+?>|\\n", " ", text)).strip()
