89c8c0186e099b9534f194cb0173f930ab11249a
cvzi <cuzi@openmail.cc>
2025-01-25 00:16:54 +0100

    remove unnecessary import

--- mensenat/__init__.py
+++ mensenat/__init__.py
@@ -8,12 +8,11 @@ import json5
 
 
 try:
-    from mensenat.tools import getMenu
     from util import xml_escape, weekdays_map
     from .canteen import Canteen
 
 except ModuleNotFoundError:
-    include = os.path.relpath(os.path.join(os.path.dirname(__file__), '..'))
+    include = os.path.relpath(os.path.join(os.path.dirname(__file__), ".."))
     sys.path.insert(0, include)
     from util import xml_escape, weekdays_map
     from canteen import Canteen
@@ -33,10 +32,9 @@ class Parser:
         uri = self.canteens[canteenReference]["source"]
         canteen = Canteen(uri)
         return canteen.genereateCurrentWeekFeedXml()
-    
+
     def feed_all(self, canteenReference: str):
         return self.feed(canteenReference)
-
 
     def meta(self, refName):
         """Generate an openmensa XML meta feed from the static json file using an XML template"""
@@ -51,15 +49,16 @@ class Parser:
                 "name": mensa["name"],
                 "address": mensa["address"],
                 "city": mensa["city"],
-                "phone": mensa['phone'],
+                "phone": mensa["phone"],
                 "latitude": mensa["latitude"],
                 "longitude": mensa["longitude"],
-                "feed": self.urlTemplate.format(metaOrFeed='feed', mensaReference=urllib.parse.quote(reference)),
+                "feed": self.urlTemplate.format(metaOrFeed="feed", mensaReference=urllib.parse.quote(reference)),
                 "source": mensa["source"],
             }
             openingTimes = {}
             pattern = re.compile(
-                r"([A-Z][a-z])(\s*-\s*([A-Z][a-z]))?\s*(\d{1,2}):(\d{2})\s*-\s*(\d{1,2}):(\d{2}) Uhr")
+                r"([A-Z][a-z])(\s*-\s*([A-Z][a-z]))?\s*(\d{1,2}):(\d{2})\s*-\s*(\d{1,2}):(\d{2}) Uhr"
+            )
             m = re.findall(pattern, mensa["times"])
             for result in m:
                 fromDay, _, toDay, fromTimeH, fromTimeM, toTimeH, toTimeM = result
@@ -89,7 +88,7 @@ class Parser:
         return '<openmensa xmlns="http://openmensa.org/open-mensa-v2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="2.1" xsi:schemaLocation="http://openmensa.org/open-mensa-v2 http://openmensa.org/open-mensa-v2.xsd"/>'
 
     def __init__(self, urlTemplate):
-        with open(metaJson, 'r', encoding='utf8') as f:
+        with open(metaJson, "r", encoding="utf8") as f:
             canteenDict = json5.load(f)
 
         self.urlTemplate = urlTemplate
@@ -102,16 +101,11 @@ class Parser:
     def json(self):
         tmp = {}
         for reference in self.canteens:
-            tmp[reference] = self.urlTemplate.format(
-                metaOrFeed='meta', mensaReference=urllib.parse.quote(reference))
+            tmp[reference] = self.urlTemplate.format(metaOrFeed="meta", mensaReference=urllib.parse.quote(reference))
         return json.dumps(tmp, indent=2)
-    
-
-
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    print(Parser(
-        "http://localhost/{metaOrFeed}/mensenat_{mensaReference}.xml").feed("WiTUSchroedingerFreihaus"))
+    print(Parser("http://localhost/{metaOrFeed}/mensenat_{mensaReference}.xml").feed("WiTUSchroedingerFreihaus"))
     # print(Parser("http://localhost/{metaOrFeed}/mensenat_{mensaReference}.xml").meta("EisenstadtFH"))
